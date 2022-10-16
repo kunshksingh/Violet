2dac7158c3dea66dc4eca1d3249edfa6d031e3f3
kunshksingh <kunshksingh@gmail.com>
2022-10-16 00:49:53 -0400

    Fix in app.py
    Fix in app.py

--- emotion.py
+++ emotion.py
@@ -2,12 +2,12 @@ import os
 import openai
 
 class Emotion:
-    def emotionalDistribution(self, characteristics):
+    def emotionalDistribution(self, distribution):
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
         emotion = openai.Completion.create(
             model="text-davinci-002",
-            prompt="The following is a probability distribution of emotions experienced by a therapist in the past:\n\n{ \"compassionate\": 0.5, \"love\":0.5}\n\nConsider possibly adding a low degree of an extreme including, which may be one of fear, excitement, frustration, depression, ecstasy, or thrill, in hopes of improving the therapist's response to a patient. However, the therapist retains mostly a playful, bubbly, strong-willed, and youthful energy.  Always consider the probability distribution of emotions: compute the emotion that the therapist feels right now. Use complex verbiage and determine the concealed emotion of the therapist in thorough detail.",
+            prompt="The following is a probability distribution of emotions experienced by a therapist in the past:\n\n"+distribution+"\n\nConsider possibly adding a low degree of an extreme emotion including, which may be one of fear, excitement, frustration, depression, ecstasy, or thrill, in hopes of improving the therapist's response to a patient. However, the therapist retains mostly a playful, bubbly, strong-willed, and youthful energy.  Always consider the probability distribution of emotions: compute the emotion that the therapist feels right now. Use complex verbiage and determine the concealed emotion of the therapist in thorough detail.",
             temperature=0.9,
             max_tokens=256,
             top_p=1,
@@ -18,4 +18,3 @@ class Emotion:
     #TODO Create another method that creates voice fluctuations based on emotion: includes breaths/softness in voice/inflections
     def voiceModulater(self):
         pass
-    
