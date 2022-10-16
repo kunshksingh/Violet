a73aea7380a86af87bce234fe19300853786b65c
kunshksingh <kunshksingh@gmail.com>
2022-10-15 21:17:09 -0400

    Improved front-end aesthetic and created base functionalities
    1. Created 5 .html templates (about.html/home.html are unfinished)
    2. Created violet animation using OpenAI Codex
    3. Setup app.py

--- app.py
+++ app.py
@@ -11,8 +11,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 @app.route('/')
 def home():
-    
-    return render_template('index.html')  # return a string
+    return render_template('index.html') 
+# ...
+
+@app.route('/login/')
+def login():
+    return render_template('login.html') 
+
+@app.route('/about/')
+def about():
+    return render_template('about.html') 
+
+@app.route('/access/')
+def access():
+    return render_template('access.html') 
 
 # Start the development server using the run() method
 if __name__ == '__main__':
