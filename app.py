ddfa371158fd90b07ef574a7a0828a53220c8dfd
kunshksingh <kunshksingh@gmail.com>
2022-10-16 09:33:32 -0400

    Added JQuery/AJAX integration
    1. Retrieves conversation data from website and outputs into app.py's conversations
    2. Working on adding delay (lock text essentially), and adding conversation back
    3. Need to remove URL redirect when creating POST requests

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 conversation = []
 times = []
 texts = dict(zip(conversation, times))
+converation = []
 # Use the decorator pattern to
 # link the view function to a url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -15,48 +16,26 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def home():
     return render_template('home.html') 
 
-#@app.route('/', methods=["GET", "POST"])
 @app.route('/')
 def index():
-    conversation = []
-    #rf = request.json
-    #print(rf)
-    #for key in rf.keys():
-        #data = key
-    #print(data)
-    #data_dic = json.loads(data)
-    #print(data_dic.keys())
-    #sum_data = data_dic['sum']
-   #if request.method == "POST":
-    msgs = request.form.get('msgs')
-    print(msgs)
-    #Message = {"Message": msgs}
-    return render_template('index.html', msgouts=msgs) 
+    return render_template('index.html',{"conversation":conversation}) 
+
+@app.route('/sendmessage', methods=["POST"])
+def sendmessage():
+    data = request.get_json()
+    data = str(data)
+    data = data[data.find("lastmsg=")+8:]
+    data = data[:data.find("'")]
+    data = data.replace("+"," ")
+    conversation.append("data")
+    return "E"
 
 
+@app.route('/', methods=["POST"])
+def index_form():
+    print(request.form['lastmsg'])
+    return render_template('index.html') 
 
-    # userText = request.args.get("lastmsg")
-    # print(userText)
-    #print("referes")
-
-
-
-
-#@app.route('/', methods=['GET', 'POST'])
-#def index_form():
-    #print(request.form['lastmsg'])
-    #userText = request.args.get('input')
-    #print(userText)
-    #return render_template('index.html') 
-'''
-@app.route('/', methods=['POST'])
-def get_response():
-    #print(request.form['lastmsg'])
-    userText = request.get['lastmsg']
-    print(userText)
-    print("efefefs")
-    return
-'''
 @app.route('/login/')
 def login():
     return render_template('login.html') 
@@ -70,5 +49,5 @@ def access():
     return render_template('access.html') 
 
 # Start the development server using the run() method
-if __name__ == 'main':
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
